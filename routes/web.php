76c781ad27e0ad3734e2935ed3a52f0f42075147
Masaya-com <masaya.0630.a@gmail.com>
2025-11-26 17:43:24 +0900

    test

--- routes/web.php
+++ routes/web.php
@@ -54,6 +54,5 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
 // ▼▼ Breeze デフォルト認証ルート（login / register など） ▼▼
 require __DIR__.'/auth.php';
